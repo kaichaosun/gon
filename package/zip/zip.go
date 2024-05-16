8e5e1b47a2920a23f9300ebdd6f0cfe129e68e99
kaichaosun <kaichaosuna@gmail.com>
2024-05-16 17:40:41 +0800

    app name when zip

--- package/zip/zip.go
+++ package/zip/zip.go
@@ -132,6 +132,21 @@ func createRoot(ctx context.Context, logger hclog.Logger, opts *Options) (string
 		return "", err
 	}
 
+	// further nest the files into another folder with the
+	// same name as the app.
+	// This ensures zip files contain the app, rather than the contents
+	// of the app folder.
+	// see https://github.com/mitchellh/gon/issues/18
+	appDir := root
+	if len(opts.Files) == 1 {
+		appName := filepath.Base(opts.Files[0])
+		appDir = filepath.Join(root, appName)
+		err = os.MkdirAll(appDir, 0777)
+		if err != nil {
+			return "", err
+		}
+	}
+
 	// Setup our args to copy our files into the root
 	cmd.Args = []string{
 		filepath.Base(cmd.Path),
